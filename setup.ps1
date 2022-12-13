d2c086974aa9c8223323a5693bac8170314f0553
Caio Barbieri <cbarbieri@vertigo.com.br>
2022-12-12 23:06:07 -0300

    more verbose

--- setup.ps1
+++ setup.ps1
@@ -1,18 +1,26 @@
+Write-Output "Verifying execution policies..."
 $Execution_LocalMachine = Get-ExecutionPolicy LocalMachine
-Write-Output $Execution_LocalMachine
 if ($Execution_LocalMachine -ne "RemoteSigned"){
+    Write-Output "Setting LocalMachine execution policy to RemoteSigned"
     Set-ExecutionPolicy RemoteSigned -Scope LocalMachine
 }
 $Execution_CurrentUser = Get-ExecutionPolicy CurrentUser
 if ($Execution_CurrentUser -ne "RemoteSigned"){
+    Write-Output "Setting CurrentUser execution policy to RemoteSigned"
     Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 }
 $Execution_Process = Get-ExecutionPolicy Process
 if ($Execution_Process -ne "RemoteSigned"){
+    Write-Output "Setting Process execution policy to RemoteSigned"
     Set-ExecutionPolicy RemoteSigned -Scope Process
 }
 
 ./update
+
+$hasWSL2 = wsl -l -v | find "2"
+if(!$hasWSL2){
+    ./wsl
+}
 
 $hasPackageManager = Get-AppPackage -name 'Microsoft.DesktopAppInstaller'
 if(!$hasPackageManager)
@@ -29,5 +37,5 @@ if(!$hasGitInstalled)
 git clone https://github.com/LeDragoX/Win-Debloat-Tools.git
 cd Win-Debloat-Tools
 Set-ExecutionPolicy Unrestricted -Scope CurrentUser -Force; ls -Recurse *.ps*1 | Unblock-File; .\"Win10ScriptCLI.ps1"
-
-./wsl
+cd ..
+del Win-Debloat-Tools
--- wsl.ps1
+++ wsl.ps1
@@ -1,3 +1,4 @@
+Write-Output "Checking if WSL is Enabled..."
 $hasWSL = (Get-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online) -Like "Enabled"
 if(!$hasWSL){
     dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
@@ -5,6 +6,7 @@ if(!$hasWSL){
     Restart-Computer -Confirm
 }
 
+Write-Output "Checking if VirtualMachinePlatform is Enabled..."
 $hasVirtualMachinePlatform = (Get-WindowsOptionalFeature -FeatureName VirtualMachinePlatform -Online) -Like "Enabled"
 if(!$hasVirtualMachinePlatform){
     dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart 
@@ -12,6 +14,7 @@ if(!$hasVirtualMachinePlatform){
     Restart-Computer -Confirm
 }
 
+Write-Output "Checking if WSL is version 2"
 $hasWSL2 = wsl -l -v | find "2"
 if(!$hasWSL2){
     $WSL_Update_URI = https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi
