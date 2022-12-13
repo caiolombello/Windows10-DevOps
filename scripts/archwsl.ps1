e9fc323dbe2b06adf5c3cd747218ba46f9eb1463
Caio Barbieri <cbarbieri@vertigo.com.br>
2022-12-13 01:01:28 -0300

    remove change file path

--- scripts/archwsl.ps1
+++ scripts/archwsl.ps1
@@ -3,8 +3,5 @@ Invoke-WebRequest -Uri $ArchWSL_URI -OutFile Arch.zip
 Move-Item -Path .\Arch.zip -Destination C:\Windows\Temp\Arch.zip
 New-Item -Path C:\Arch -ItemType Directory
 Expand-Archive -Path C:\Windows\Temp\Arch.zip -DestinationPath C:\Arch
-$path = Get-Location
-Set-Location -Path C:\Arch
-.\arch
-Set-Location -Path $path
+Start-Process -FilePath C:\Arch\arch
 wsl -d Arch comando-aqui
